6cea7310c4abf461c5ec5a6952430c878034b0d2
Kyhan <kyhanb@icloud.com>
2023-11-15 18:02:13 +1000

    Barely anything help im held here against my will

--- TopDownGame/Assets/Scripts/CameraFollow.cs
+++ TopDownGame/Assets/Scripts/CameraFollow.cs
@@ -7,7 +7,7 @@ public class CameraFollow : MonoBehaviour
     public float DelayRadius                                                                                                                                                                                                                                                        ;
     public GameObject Player                                                                                                                                                                                                                                                        ;
     private Vector2 PlayerPos                                                                                                                                                                                                                                                       ;
-    [Range(0f, 0.5f)] public float CAmera2                                                                                                                                                                                                                                             ;
+    [Range(0f, 0.5f)] public float CAmera2                                                                                                                                                                                                                                          ;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +17,12 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetComponent<Camera>().orthographicSize += Input.mouseScrollDelta.y                                                                                                                                                                                                           ;
         if (Vector2.Distance(transform.position, Player.transform.position) > DelayRadius)
         {
             PlayerPos = Player.transform.position                                                                                                                                                                                                                                     ;
             //Vector3 vector3 = transform.position - Player.transform.position                                                                                                                                                                                                        ;
-            this.transform.position = Vector2.Lerp(transform.position, PlayerPos, CAmera2)                                                                                                                                                                                             ;        
+            this.transform.position = Vector2.Lerp(transform.position, PlayerPos, CAmera2)                                                                                                                                                                                            ;        
         }
         // Vector3 newpos = new Vector3(Player.transform.position.x, Player.transform.position.y, this.transform.position.z)                                                                                                                                                          ;
         //this.transform.position = newpos                                                                                                                                                                                                                                            ;
